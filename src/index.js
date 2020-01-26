b48a1e516f727106ff362c20ea3fff328bc90541
matryoshka <nazardjali@gmail.com>
2020-01-26 22:02:01 +0200

    Init

---
+++ src/index.js
@@ -0,0 +1,63 @@
+import PhotoSwipe from 'photoswipe/dist/photoswipe'
+import PhotoSwipeUI_Default from 'photoswipe/dist/photoswipe-ui-default'
+import 'photoswipe/dist/default-skin/default-skin.css'
+import 'photoswipe/dist/photoswipe.css'
+
+var pswpElement = document.querySelectorAll('.pswp')[0];
+
+// build items array
+var items = [
+    {
+        src: 'https://placekitten.com/600/400',
+        w: 600,
+        h: 400
+    },
+    {
+        src: 'https://placekitten.com/1200/900',
+        w: 1200,
+        h: 900
+    }
+];
+
+//
+
+fetch('http://localhost:5000/get-current-dir-image-names-recursively')
+    .then(r => r.json())
+    .then(j => {
+        const images = j.map(imageName => {
+            return {
+                src: `http://localhost:5000/get-image/${imageName}`,
+                w: 0,
+                h: 0
+            }
+        });
+
+        // define options (if needed)
+        const options = {
+            // optionName: 'option value'
+            // for example:
+            index: 0 // start at first slide
+        };
+
+        const gallery = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default, images, options);
+
+        gallery.listen('gettingData', function(index, item) {
+            if (item.w < 1 || item.h < 1) { // unknown size
+                const img = new Image();
+                img.onload = function() { // will get size after load
+                    item.w = this.width; // set image width
+                    item.h = this.height; // set image height
+                    gallery.updateSize(true);
+                };
+                img.src = item.src; // let's download image
+            }
+        });
+
+        // Initializes and opens PhotoSwipe
+        gallery.init();
+    });
+
+
+
+
+
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ],
+            },
+            {
+                test: /\.svg$/,
+                loader: 'svg-inline-loader'
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                    },
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'src/index.html'
+        })
+    ],
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: false,
+        port: 9000
+    }
+
+};
