5628576ca2fc37d2def7038d722aeb62fc6815b1
matryoshka <nazardjali@gmail.com>
2020-01-28 04:53:08 +0200

    Add eslint

--- src/index.jsx
+++ src/index.jsx
@@ -1,68 +1,64 @@
-import PhotoSwipe from 'photoswipe/dist/photoswipe'
-import PhotoSwipeUI_Default from 'photoswipe/dist/photoswipe-ui-default'
-import 'photoswipe/dist/default-skin/default-skin.css'
-import 'photoswipe/dist/photoswipe.css'
-import ReactDOM from 'react-dom'
-import React from 'react'
+import PhotoSwipe from 'photoswipe/dist/photoswipe';
+import { PhotoSwipeUI_Default as PhotoSwipeUIDefault } from 'photoswipe/dist/photoswipe-ui-default';
+import 'photoswipe/dist/default-skin/default-skin.css';
+import 'photoswipe/dist/photoswipe.css';
+import ReactDOM from 'react-dom';
+import React from 'react';
 
-var pswpElement = document.querySelectorAll('.pswp')[0];
+const pswpElement = document.querySelectorAll('.pswp')[0];
 
 // build items array
-var items = [
-    {
-        src: 'https://placekitten.com/600/400',
-        w: 600,
-        h: 400
-    },
-    {
-        src: 'https://placekitten.com/1200/900',
-        w: 1200,
-        h: 900
-    }
+const items = [
+  {
+    src: 'https://placekitten.com/600/400',
+    w: 600,
+    h: 400,
+  },
+  {
+    src: 'https://placekitten.com/1200/900',
+    w: 1200,
+    h: 900,
+  },
 ];
 
 //
 
 fetch('http://localhost:5000/get-current-dir-image-names-recursively')
-    .then(r => r.json())
-    .then(j => {
-        const images = j.map(imageName => {
-            return {
-                src: `http://localhost:5000/get-image/${imageName}`,
-                w: 0,
-                h: 0
-            }
-        });
+  .then((r) => r.json())
+  .then((j) => {
+    const images = j.map((imageName) => ({
+      src: `http://localhost:5000/get-image/${imageName}`,
+      w: 0,
+      h: 0,
+    }));
 
-        // define options (if needed)
-        const options = {
-            // optionName: 'option value'
-            // for example:
-            index: 0 // start at first slide
+    // define options (if needed)
+    const options = {
+      // optionName: 'option value'
+      // for example:
+      index: 0, // start at first slide
+    };
+
+    const gallery = new PhotoSwipe(pswpElement, PhotoSwipeUIDefault, items, options);
+
+    gallery.listen('gettingData', (index, item) => {
+      const image = item;
+      if (image.w < 1 || image.h < 1) { // unknown size
+        const img = new Image();
+        img.onload = () => { // will get size after load
+          image.w = this.width; // set image width
+          image.h = this.height; // set image height
+          gallery.updateSize(true);
         };
-
-        const gallery = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default, images, options);
-
-        gallery.listen('gettingData', function (index, item) {
-            if (item.w < 1 || item.h < 1) { // unknown size
-                const img = new Image();
-                img.onload = function () { // will get size after load
-                    item.w = this.width; // set image width
-                    item.h = this.height; // set image height
-                    gallery.updateSize(true);
-                };
-                img.src = item.src; // let's download image
-            }
-        });
-
-        // Initializes and opens PhotoSwipe
-        gallery.init();
+        img.src = item.src; // let's download image
+      }
     });
 
+    // Initializes and opens PhotoSwipe
+    gallery.init();
+  });
+
 ReactDOM.render(
-    <h1>Hello, world!</h1>,
-    document.getElementById('root')
+  <h1>Hello, world!</h1>,
+  document.getElementById('root'),
 );
-
-
-
--- webpack.config.js
+++ webpack.config.js
@@ -2,53 +2,62 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.jsx',
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(m?js|jsx)$/,
-                exclude: /(node_modules|bower_components|dist)/,
-                use: {
-                  loader: 'babel-loader',
-                  options: {
-                    presets: ['@babel/preset-env', ['@babel/preset-react', {'development': true}]]
-                  }
-                }
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    'style-loader',
-                    'css-loader'
-                ],
-            },
-            {
-                test: /\.svg$/,
-                loader: 'svg-inline-loader'
-            },
-            {
-                test: /\.(png|jpe?g|gif)$/i,
-                use: [
-                    {
-                        loader: 'file-loader',
-                    },
-                ],
-            },
+  entry: './src/index.jsx',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.(m?js|jsx)$/,
+        exclude: /(node_modules|bower_components|dist)/,
+        loader: 'eslint-loader',
+        options: {
+          emitWarning: true,
+        },
+      },
+      {
+        test: /\.(m?js|jsx)$/,
+        exclude: /(node_modules|bower_components|dist)/,
+        use: [{
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', ['@babel/preset-react', { development: true }]],
+          },
+        }],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
         ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: 'src/index.html'
-        })
+      },
+      {
+        test: /\.svg$/,
+        loader: 'svg-inline-loader',
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
     ],
-    devServer: {
-        contentBase: path.join(__dirname, 'dist'),
-        compress: false,
-        port: 9000
-    }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+    }),
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: false,
+    port: 9000,
+  },
 
 };
