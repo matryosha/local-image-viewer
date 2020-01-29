95f5e4f6faba5edec2ec3561143626de90a663c3
matryoshka <nazardjali@gmail.com>
2020-01-29 17:34:22 +0200

    Add ButtonBase

---
+++ src/Components/ButtonBase.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Styles/button-base.sass';
+
+
+export default function ButtonBase(props) {
+  const { children, onClick, styleClassNames } = props;
+  const classNames = `btn-base ${styleClassNames !== undefined ? styleClassNames : ''}`;
+  return (
+    <div className={classNames} onClick={onClick}>
+      {children}
+    </div>
+  );
+}
