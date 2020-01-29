0497cec4e28583723a13f510ce377bf3b9f349af
matryoshka <nazardjali@gmail.com>
2020-01-29 17:35:51 +0200

    Add RootButton

---
+++ src/Components/RootButton.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ButtonBase from './ButtonBase';
+import './Styles/button-root.sass';
+
+export default function RootButton() {
+  return (
+    <ButtonBase>
+      <div className="btn-root">
+        <svg width="20" height="35" viewBox="0 0 20 35" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M2 34L18 1" stroke="#8036A3" strokeWidth="4" />
+        </svg>
+      </div>
+    </ButtonBase>
+  );
+}
