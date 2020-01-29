3ee1a1f6cecbe891e7b390ea1b3ad3c84e633e6d
matryoshka <nazardjali@gmail.com>
2020-01-29 17:31:51 +0200

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,4 +5,14 @@ module.exports = {
     browser: true,
     node: true,
   },
+  overrides: [
+    {
+      files: ["*.jsx", "*.js"],
+      rules: {
+        "react/prop-types": "off",
+        "jsx-a11y/click-events-have-key-events": "off",
+        "jsx-a11y/no-static-element-interactions": "off",
+      },
+    },
+  ],
 };
