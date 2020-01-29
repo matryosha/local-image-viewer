d928cf09cdba88188d29f54d3dd8fc6ffd80e741
matryoshka <nazardjali@gmail.com>
2020-01-29 17:35:34 +0200

    Add FolderUpButton

---
+++ src/Components/FolderUpButton.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ButtonBase from './ButtonBase';
+import './Styles/button-folder-up.sass';
+
+export default function ButtonFolderUp() {
+  return (
+    <ButtonBase>
+      <div className="btn-folder-up">
+        <svg width="20" height="37" viewBox="0 0 20 37" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M18 2L10.5 10.25L3 18.5L18 35" stroke="#357BA3" strokeWidth="4" />
+        </svg>
+      </div>
+    </ButtonBase>
+  );
+}
