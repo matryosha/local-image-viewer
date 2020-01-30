d8a9993d2962b9cd97e319be5e8e443cb5d1d3cb
matryoshka <nazardjali@gmail.com>
2020-01-30 17:40:08 +0200

    Update ButtonBase.jsx

--- src/Components/ButtonBase.jsx
+++ src/Components/ButtonBase.jsx
@@ -6,7 +6,7 @@ export default function ButtonBase(props) {
   const { children, onClick, styleClassNames } = props;
   const classNames = `btn-base ${styleClassNames !== undefined ? styleClassNames : ''}`;
   return (
-    <div className={classNames} onClick={onClick}>
+    <div className={classNames} onClick={async () => onClick()}>
       {children}
     </div>
   );
