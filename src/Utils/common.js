ae686d0e28358afc89bb1ded4080eeb2e9b17bde
matryoshka <nazardjali@gmail.com>
2020-01-30 23:11:32 +0200

    Update common.js

--- src/Utils/common.js
+++ src/Utils/common.js
@@ -1,4 +1,8 @@
-export default function compareItems(item1, item2) {
+/**
+ * Used as compare function in filter items methods.
+ * Orders items in such way: All items will be sorted alphabetical with folder at top.
+ */
+function compareItems(item1, item2) {
   if (item1.isFile && !item2.isFile) {
     return 1;
   }
@@ -15,3 +19,23 @@ export default function compareItems(item1, item2) {
   }
   return 0;
 }
+
+function transformToRelImageUrl(item, currentDirString, getImageEndpoint) {
+  if (currentDirString === '') return `${getImageEndpoint}/${currentDirString}${item.name}`;
+  return `${getImageEndpoint}/${currentDirString}/${item.name}`;
+}
+
+function createGalleryItemList(currentDirString, dirItems, apiService) {
+  const { getImageEndpoint } = apiService;
+  const currentDirFilesOnly = dirItems.filter((i) => i.isFile);
+
+  const filesWithRelativeUrl = currentDirFilesOnly
+    .map((item) => transformToRelImageUrl(item, currentDirString, getImageEndpoint));
+
+  return filesWithRelativeUrl.map((f) => ({ src: f, w: -1, h: -1 }));
+}
+
+export {
+  compareItems,
+  createGalleryItemList,
+};
