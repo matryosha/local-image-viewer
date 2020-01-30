a47f847a189d49f29e587cc9551420ccc7f08522
matryoshka <nazardjali@gmail.com>
2020-01-30 15:48:15 +0200

    Add galleryService.js

---
+++ src/Services/galleryService.js
@@ -0,0 +1,31 @@
+import 'photoswipe/dist/default-skin/default-skin.css';
+import 'photoswipe/dist/photoswipe.css';
+import PhotoSwipe from 'photoswipe/dist/photoswipe';
+// eslint-disable-next-line camelcase
+import PhotoSwipeUI_Default from 'photoswipe/dist/photoswipe-ui-default';
+
+export default () => ({
+  open(items) {
+    const pswpElement = document.querySelectorAll('.pswp')[0];
+
+    const options = {
+      index: 0,
+    };
+
+    const gallery = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default, items, options);
+
+    gallery.listen('gettingData', (index, item) => {
+      if (item.w < 1 || item.h < 1) {
+        const img = new Image();
+        img.onload = function () {
+          item.w = this.width;
+          item.h = this.height;
+          gallery.invalidateCurrItems();
+          gallery.updateSize(true);
+        };
+        img.src = item.src;
+      }
+    });
+    gallery.init();
+  },
+});
