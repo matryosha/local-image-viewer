b05905b0d773a48ecfd0379ec94152e9de97ad52
matryoshka <nazardjali@gmail.com>
2020-02-01 22:33:47 +0200

    Update currentDirTransform.js
    Added pathNameToCurrentDirString function

--- src/Utils/currentDirTransform.js
+++ src/Utils/currentDirTransform.js
@@ -14,7 +14,16 @@ function dirUp(currentDirString) {
   return currentDirString.slice(0, lastSlashIndex);
 }
 
+function pathNameToCurrentDirString(pathName) {
+  const result = pathName.slice(1);
+  const firstSlashIndex = pathName.indexOf('/');
+  if (firstSlashIndex === 0) return result;
+
+  return result;
+}
+
 export {
   openDir,
   dirUp,
+  pathNameToCurrentDirString,
 };
