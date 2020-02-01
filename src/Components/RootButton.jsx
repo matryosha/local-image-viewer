63356b298de3713bc3844935bef836221abf8379
matryoshka <nazardjali@gmail.com>
2020-02-01 22:42:35 +0200

    Update RootButton.jsx
    Added handling onClick prop

--- src/Components/RootButton.jsx
+++ src/Components/RootButton.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 import ButtonBase from './ButtonBase';
 import './Styles/button-root.sass';
 
-export default function RootButton() {
+export default function RootButton(props) {
+  const { onClick } = props;
   return (
-    <ButtonBase>
+    <ButtonBase onClick={async () => onClick()}>
       <div className="btn-root">
         <svg width="20" height="35" viewBox="0 0 20 35" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path d="M2 34L18 1" stroke="#8036A3" strokeWidth="4" />
