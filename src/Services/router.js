e6afeba58060acf401ea74bcdae273f279008d7d
matryoshka <nazardjali@gmail.com>
2020-02-01 22:26:44 +0200

    Add router.js

---
+++ src/Services/router.js
@@ -0,0 +1,28 @@
+export default {
+  handlers: [],
+  init() {
+    window.onpopstate = (e) => this.onPopStateHandler(e);
+  },
+  onPopStateHandler(e) {
+    this.notifyHistoryChanged(e);
+  },
+  notifyHistoryChanged(event) {
+    this.handlers.forEach((h) => {
+      h(event.state?.path);
+    });
+  },
+  subscribeToHistoryChanges(fn) {
+    this.handlers.push(fn);
+  },
+  goTo(path, relative = false) {
+    const urlToGo = relative ? path : `/${path}`;
+    window.history.pushState({ path }, '', urlToGo);
+  },
+  goUp(path) {
+    window.history.pushState(
+      { path },
+      '',
+      path === '' ? '/' : `/../${path}`,
+    );
+  },
+};
