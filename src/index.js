dff3c2868d4588b2ab0877507091d70f08a02cc2
matryoshka <nazardjali@gmail.com>
2020-02-01 22:27:13 +0200

    Update index.js
    Added router service

--- src/index.js
+++ src/index.js
@@ -3,14 +3,17 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 import GalleryService from './Services/galleryService';
 import ApiService from './Services/api';
+import RouterService from './Services/router';
 import App from './App';
 
 const apiService = ApiService;
 const galleryService = GalleryService();
+const routerService = RouterService;
 
 const services = {
   apiService,
   galleryService,
+  routerService,
 };
 
 ReactDOM.render(
